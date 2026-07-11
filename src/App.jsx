dd83d002bbb0ea1fb8d9948fbc35072c4ee9fbce
anan531 <ananyat612@gmail.com>
2026-07-11 12:02:47 +0530

    Delete added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import heroImg from './assets/hero.png'
 import './App.css'
 import AddReg from './components/AddReg'
 import SearchReg from './components/SearchReg'
+import DelReg from './components/DelReg'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,7 @@ function App() {
     <>
       <AddReg/>
       <SearchReg/>
+      <DelReg/>
     </>
   )
 }
---
+++ src/components/DelReg.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const DelReg = () => {
+  return (
+    <div>
+
+
+<h1>Delete Registration</h1>
+<div className="container">
+    <div className="row g-3">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+<div className="container">
+    <div className="row g-3">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+
+<label htmlFor="" className="form-label">Registration No</label>
+<input type="text" className="form-control" />
+
+        </div>
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+
+<button className="btn btn-success">DELETE</button>
+
+        </div>
+    </div>
+</div>
+
+        </div>
+    </div>
+</div>
+
+    </div>
+  )
+}
+
+export default DelReg
