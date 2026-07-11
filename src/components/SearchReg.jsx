8ceb993d18ad2f9bd49fa1c9510d7dbf58349f7c
anan531 <ananyat612@gmail.com>
2026-07-11 11:56:12 +0530

    Search added

---
+++ src/components/SearchReg.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const SearchReg = () => {
+  return (
+    <div>
+<h1>Search Registration</h1>
+<div className="container">
+    <div className="row g-3">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+<div className="container">
+    <div className="row g-3">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+
+<label htmlFor="" className="form-label">Registration No</label>
+<input type="text" className="form-control" />
+
+        </div>
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+
+<button className="btn btn-success">SEARCH</button>
+
+        </div>
+    </div>
+</div>
+
+        </div>
+    </div>
+</div>
+
+    </div>
+  )
+}
+
+export default SearchReg
