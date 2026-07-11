3a49347d16293ccfd1209a42cf192ed499b15641
anan531 <ananyat612@gmail.com>
2026-07-11 11:27:50 +0530

    created'

---
+++ src/components/AddReg.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const AddReg = () => {
+  return (
+    <div>
+
+<h1>Add Registration Page</h1>
+{/* <div className="container">
+    <div className="row g-3">
+        <div className="col col-12 col-sm-12 col-md-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+
+
+        </div>
+    </div>
+</div> */}
+
+    </div>
+  )
+}
+
+export default AddReg
